ff5c25b7c333d3a0a7c5cb367954220be4b9157a
bwkw <na.shota.1139@gmail.com>
2023-05-12 19:33:48 -0700

    :sparkles: Load prisma module from other module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Request } from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL ?? 'http://localhost:3000'],
+  });
   await app.listen(3005);
 }
 bootstrap();
